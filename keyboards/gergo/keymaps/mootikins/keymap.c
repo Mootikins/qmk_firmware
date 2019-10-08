abea2f2e2782acba5e67309d429307e187ed2e2b
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-10-08 14:32:55 -0600

    swapped parens and braces

--- keyboards/gergo/keymaps/mootikins/keymap.c
+++ keyboards/gergo/keymaps/mootikins/keymap.c
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                                   ,-------------------------------------------.
  * |  TAB   |   Q  |   W  |   E  |   R  |   T  |                                   |   Y  |   U  |   I  |   O  |   P  |  - _   |
  * |--------+------+------+------+------+------|------.                     .------|------+------+------+------+------+--------|
- * |  CTRL  |   A  |   S  |  D   |   F  |   G  |  [   |                     |   ]  |   H  |   J  |   K  |   L  | ;  : |  ' "   |
+ * |  CTRL  |   A  |   S  |  D   |   F  |   G  |  (   |                     |   )  |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------|------|                     |------|------+------+------+------+------+--------|
- * |  SHIFT |   Z  |   X  |   C  |   V  |   B  |  (   | ,-------. ,-------. |   )  |   N  |   M  | ,  < | . >  | /  ? |  = +   |
+ * |  SHIFT |   Z  |   X  |   C  |   V  |   B  |  [   | ,-------. ,-------. |   ]  |   N  |   M  | ,  < | . >  | /  ? |  = +   |
  * `--------------------------------------------------' |  DEL  | |  ESC  | `--------------------------------------------------'
  *                        .------.   ,-------.   ,------|-------| |-------|------.   ,-------.   ,------.
  *                        | LALT |   |  MOD  |   |      |       | |       |      |   |   [   |   |   ]  |
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_gergo(
     KC_TAB,  KC_Q, KC_W, KC_E, KC_R,    KC_T,                                                     KC_Y,     KC_U,   KC_I,    KC_O,   KC_P,    KC_MINS,
-    KC_LCTL, KC_A, KC_S, KC_D, KC_F,    KC_G,    KC_LBRC,                                KC_RBRC, KC_H,     KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOTE,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V,    KC_B,    KC_LPRN, KC_DEL,                KC_ESC, KC_RPRN, KC_N,     KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_EQL,
+    KC_LCTL, KC_A, KC_S, KC_D, KC_F,    KC_G,    KC_LPRN,                                KC_RPRN, KC_H,     KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOTE,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V,    KC_B,    KC_LBRC, KC_DEL,                KC_ESC, KC_RBRC, KC_N,     KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_EQL,
                                KC_LALT, KC_LGUI, KC_BSPC,  TT(NUMB),     LT(EXT,KC_ENT), KC_SPC,  KC_GRAVE, KC_BSLASH
     ),
 /* Keymap 1: Pad/Function layer
